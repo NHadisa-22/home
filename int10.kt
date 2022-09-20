d9625eac1e439dde96c9f898c5fff9f68e873513
NHadisa-22 <112709712+NHadisa-22@users.noreply.github.com>
2022-09-20 17:31:40 +0300

    Create int10.kt

---
+++ int10.kt
@@ -0,0 +1,12 @@
+fun int10(a:Int) { //
+    var b:Int=a % 10
+    var c:Int=a%100/10
+
+    println("Еденицы = $b")
+    println("Десятки = $c")
+
+}
+fun main(args: Array<String>) {
+    int7(150)
+
+}
