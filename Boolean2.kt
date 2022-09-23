8ce0131c5fa5422eb5170dcaa24a3e53592cdd7f
NHadisa-22 <112709712+NHadisa-22@users.noreply.github.com>
2022-09-23 07:49:35 +0300

    Create Boolean2.kt

---
+++ Boolean2.kt
@@ -0,0 +1,11 @@
+fun Boolean1(A:Int){
+    if (A%2!=0){
+        println("Число является нечетным")
+    }
+    else {
+        println("Число является четным")
+    }
+}
+fun main(args: Array<String>){
+    Boolean1(5)
+}
