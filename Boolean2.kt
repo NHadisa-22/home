77c045bdbcefc4c2f6ed43446f738008d0fffb31
NHadisa-22 <112709712+NHadisa-22@users.noreply.github.com>
2022-09-23 08:40:05 +0300

    Update Boolean2.kt

--- Boolean2.kt
+++ Boolean2.kt
@@ -1,11 +1,11 @@
-fun Boolean1(A:Int){
+fun Boolean2(A:Int){
     if (A%2!=0){
-        println("Число является нечетным")
+        println("Число является нечетным (Истина)")
     }
     else {
-        println("Число является четным")
+        println("Число является четным (Лож)")
     }
 }
 fun main(args: Array<String>){
-    Boolean1(5)
+    Boolean2(5)
 }
