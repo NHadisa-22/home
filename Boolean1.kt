79bd89165cf91375c063764467c7c50e3c0665cf
NHadisa-22 <112709712+NHadisa-22@users.noreply.github.com>
2022-09-23 07:25:08 +0300

    Create Boolean1.kt

---
+++ Boolean1.kt
@@ -0,0 +1,11 @@
+fun Boolean1(A:Int){  //где A>0
+    if (A>0){
+        println("Число является положительным")
+    }
+    else {
+        println("Число является отрецательным")
+    }
+}
+fun main(args: Array<String>){
+    Boolean1(-1)
+}
